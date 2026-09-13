6890cdb19c274793f98d85118f5aefac79e448d5
Ragini Gupta <ragini15g@gmail.com>
2026-09-13 22:24:46 +0530

    Initial project setup

---
+++ src/main/java/com/solemate/SolemateApplication.java
@@ -0,0 +1,13 @@
+package com.solemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolemateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solemate/SolemateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
